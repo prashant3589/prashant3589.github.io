9c45fd3946e30e7226a5ae34d553047855194bb2
prashant3589 <38283849+prashant3589@users.noreply.github.com>
2025-02-16 10:20:44 +0530

    Create game.js

---
+++ game.js
@@ -0,0 +1,47 @@
+let x = 50;
+let y = 50;
+const speed = 5;
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Update position
+    x += speed;
+    if (x > canvas.width) {
+        x = 0;
+    }
+
+    // Draw rectangle
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, y, 50, 50);
+}
+
+
+document.addEventListener('keydown', function(event) {
+    switch(event.key) {
+        case 'ArrowUp':
+            y -= speed;
+            break;
+        case 'ArrowDown':
+            y += speed;
+            break;
+        case 'ArrowLeft':
+            x -= speed;
+            break;
+        case 'ArrowRight':
+            x += speed;
+            break;
+    }
+});
+
+const img = new Image();
+img.src = './image.png';
+
+img.onload = function() {
+    function update() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw image
+        ctx.drawImage(img, x, y, 50, 50);
+    }
+}
