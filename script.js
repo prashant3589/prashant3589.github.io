5ddbd53d6e2a54cbef4925e0ed4e5a933073d612
prashant3589 <38283849+prashant3589@users.noreply.github.com>
2025-05-01 10:10:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ window.addEventListener('scroll', () => {
     document.querySelectorAll('.section').forEach(section => {
       const rect = section.getBoundingClientRect();
       if(rect.top < window.innerHeight - 100){
-        section.style.opacity = 1;
-        section.style.transform = "translateY(0)";
+        //section.style.opacity = 1;
+        //section.style.transform = "translateY(0)";
+        section.style.opacity = 1 ;
+        section.style.transform = none;
       } else {
-        section.style.opacity = 0;
-        section.style.transform = "translateY(50px)";
+        //section.style.opacity = 0;
+        //section.style.transform = "translateY(50px)";
+        section.style.opacity = 1 ;
+        section.style.transform = none;
       }
     });
   });
